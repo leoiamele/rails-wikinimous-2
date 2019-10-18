03c845cadc8ec2015bd80d4d53a7903059205ac5
Leandro Iamele <leandroiamele@gmail.com>
2019-10-18 10:53:07 -0300

    adding upadate and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,35 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id])
   end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  @article = Article.create(article_params)
+  @article.save
+  redirect_to articles_path
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	@article = Article.update(article_params)
+  	redirect_to articles_path
+  end
+
+  private
+
+  def destroy
+  	@article = Article.find(params[:id])
+  	@article.destroy
+  	
+  end
+  def article_params
+	 params.require(:article).permit(:title, :content)
+  end
 end
